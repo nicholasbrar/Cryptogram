80d4abb3c7ca4ae748598f7d9f593e66ceed9575
Nicholas Brar <153023837+nicholasbrar@users.noreply.github.com>
2025-04-24 01:15:34 -0400

    Create helper_three.cpp

---
+++ src/helper_three.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+string swapCharacters(const string& input, const unordered_map<char, char>& swaps) {
+    string output = input;
+    unordered_set<char> swappedCharacters;
+
+    for (char& c : output) {
+        if (swaps.count(c) && swappedCharacters.find(c) == swappedCharacters.end()) {
+            c = swaps.at(c);
+            swappedCharacters.insert(c); // Insert the new character to the set to mark it as swapped
+        }
+    }
+    return output;
+}
+
+int main() {
+    unordered_map<char, char> charSwaps = {
+        {'A', '.'}, {'B', '.'}, {'C', '.'}, {'D', 'S'}, {'E', 'Y'}, {'F', '.'},
+        {'G', 'P'}, {'H', '.'}, {'I', 'T'}, {'J', 'Z'}, {'K', '.'}, {'L', 'E'},
+        {'M', '.'}, {'N', 'U'}, {'O', '.'}, {'P', 'G'}, {'Q', '.'}, {'R', 'V'},
+        {'S', '.'}, {'T', 'H'}, {'U', 'Q'}, {'V', 'W'}, {'W', '.'}, {'X', 'J'},
+        {'Y', 'R'}, {'Z', 'X'}
+    };
+
+    string cipher = "YLDLFYGTLYDPYMHFQNBASGNKSRLYDSIEMKAMKWSDAFKOFKKMNKGLOIMOFEITFIITLETFRLPMNKOFVFEIMINYKSYMKSKIMWMAOITLYLSDVSOLDQYLFODCLQISGSDHSKITLFGFOLHSGGMHHNKSIETMVLRLYITLDGSLKISDIDPYMHDIMKEBYMMCFYLMPPLYSKWIMWSRLILKQMNKODMPQNYLWMAOIMFKEMKLVTMFDCDPMYSIIMQYMRLITLSYQMSKIFAAIYFOSKWSKWMAOGMHQFKSLDVFDTFAILOMKITLDIMGCLZGNPKWLD";
+
+    string swapped = swapCharacters(cipher, charSwaps);
+    cout << swapped << endl;
+
+    return 0;
+}
