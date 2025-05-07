fa976de9eaf37d163cea4414e412a67b0ccdfe91
Nicholas Brar <153023837+nicholasbrar@users.noreply.github.com>
2025-05-07 02:55:47 -0400

    Formatting improvements

--- src/helper_one.cpp
+++ src/helper_one.cpp
@@ -5,20 +5,15 @@ using namespace std;
 
 int main() {
   string crypto = "QLIPX YLXDY ITQMM JINGE KGSOK RQCVS NIEKF QCWXH SFPSX KPQFK LPGIR SXENX XINXD EYJJL UQFKT JNFJM EISCC SDQCS PMEWR EGIYP QLXHN STPVQ DPXOI GDQNW YSILK KNXDF LJGLM WYGRL ZNPWA EYQNL WPQIY PVGXP CIXHA DUCEH IMEKJ FQMRG XYQQH PQCKK BMEWX VDIQO IMSOG DRTIN DPOMF SAMKO WMPPN TRJVC ITEYX MAMVC ERQML LXCIM XOAFF EAYKS KKBWC WREGG RLIFP WDDAJ VTKEA LTQCI YPVIR KULEK FPPAD D";
-
   string cleaned;
+ 
   for (char ch : crypto) {
-      if (ch != ' ') {
-          cleaned += ch;
-      }
+      if (ch != ' ') cleaned += ch;
   }
 
   crypto = cleaned; 
 
-  string first = "";
-  string second = "";
-  string third = "";
-  string fourth = "";
+  string first = "", second = "", third = "", fourth = "";
 
   for(int i = 0; i < crypto.length(); i+=4){
     if (isalpha(crypto[i])) {  
@@ -43,8 +38,6 @@ int main() {
   }
 
   crypto = cleaned_again; 
-
-
   cout << crypto << endl;
 
   return 0;
